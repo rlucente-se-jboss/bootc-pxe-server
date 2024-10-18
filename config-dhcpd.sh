616c49427b83ee95a91743ae8a57b4ec10a7a029
rlucente-se-jboss <rlucente@redhat.com>
2024-10-18 14:09:42 -0400

    explain what this is doing

--- config-dhcpd.sh
+++ config-dhcpd.sh
@@ -13,23 +13,37 @@ firewall-cmd --reload
 
 # enable configuration
 cat > /etc/dhcp/dhcpd.conf <<EOF
+# Declare the Client System Architecture Type (code 93) option as two
+# octects.
 option architecture-type code 93 = unsigned integer 16;
 
+# Provide dynamic IP Addresses for the network $SUBNET_IP/$SUBNET_MASK
 subnet $SUBNET_IP netmask $SUBNET_MASK {
+  # Packet routers on the network
   option routers $ROUTER_IP;
+
+  # DNS servers on the network
   option domain-name-servers $ROUTER_IP;
+
+  # hand out addresses in the given range
   range $SUBNET.100 $SUBNET.200;
+
+  # configuration for PXE clients using vendor-class-identifier
   class "pxeclients" {
+    # if the first nine characters match "PXEClient"
     match if substring (option vendor-class-identifier, 0, 9) = "PXEClient";
     next-server $HOSTIP;
-          if option architecture-type = 00:07 {
-            filename "redhat/EFI/BOOT/BOOTX64.EFI";
-          }
-          else {
-            filename "pxelinux/pxelinux.0";
-          }
+
+    # We will look for 00:07 (EFI Bytecode) or 00:09 (EFI x86-64) from
+    # the client
+    if option architecture-type = 00:07 or option architecture-type = 00:09 {
+      filename "redhat/EFI/BOOT/BOOTX64.EFI";
+    }
   }
+
+  # configuration for UEFI HTTP clients using vendor-class-identifier
   class "httpclients" {
+    # if the first ten characters match "HTTPClient"
     match if substring (option vendor-class-identifier, 0, 10) = "HTTPClient";
     option vendor-class-identifier "HTTPClient";
     filename "http://$HOSTIP/redhat/EFI/BOOT/BOOTX64.EFI";
